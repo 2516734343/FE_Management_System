ac93167e491f12c4e967cd313789584e60bd2e43
2516734343 <48059112+2516734343@users.noreply.github.com>
2021-02-24 09:56:05 +0800

    Delete management directory

--- management/src/remote/register.js
+++
@@ -1,6 +0,0 @@
-export async function register(params) {
-    return await fetch("/api/system/management/register", {
-        method: 'POST',
-        body: JSON.stringify(params)
-    }).then(resp => resp.json()).then(resp => resp.data);
-}
